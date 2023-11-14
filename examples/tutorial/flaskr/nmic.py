07a64d7d1fad5d41fb0b5489b77206aed6b9b5a5
alexei@proc.ru <alexei@proc.ru>
2023-11-14 13:29:18 +0300

    @work

--- examples/tutorial/flaskr/nmic.py
+++ examples/tutorial/flaskr/nmic.py
@@ -14,12 +14,12 @@ bp = Blueprint("nmic", __name__, url_prefix="/nmic")
 
 @bp.route("/nmic")
 def index():
-	# """Show all the posts, most recent first."""
-	# db = get_db()
-	# posts = db.execute(
-	# 	"SELECT p.id, title, body, created, author_id, username"
-	# 	" FROM post p JOIN user u ON p.author_id = u.id"
-	# 	" ORDER BY created DESC"
-	# ).fetchall()
-	# return render_template("nmic/index.html", posts=posts[-1:])
-	return render_template("nmic/index.html")
+	"""Show all the posts, most recent first."""
+	db = get_db()
+	posts = db.execute(
+		"SELECT p.id, title, body, created, author_id, username"
+		" FROM post p JOIN user u ON p.author_id = u.id"
+		" ORDER BY created DESC"
+	).fetchall()
+	return render_template("nmic/index.html", posts=posts[-1:])
+	# return render_template("nmic/index.html")
